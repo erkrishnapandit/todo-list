a7182a35378387b3b8469de448a799608d1acf7f
Krishna Kumar Pandit <krishnakumarji100@gmail.com>
2024-06-08 11:53:29 +0530

    Styling and functionality is added

---
+++ script.js
@@ -0,0 +1,45 @@
+// Importing nessecray DOM variables
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+// Deining Add Task Function
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    // Making input felid empty.
+    inputBox.value = '';
+    // saving data in local storage.
+    saveData();
+}
+
+// Adding event listener when the task is clicked or remove icon is clicked.
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+// Function to store data in local storage
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// Functio to get deta from the local storage
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+// Calling show data function.
+showData();
